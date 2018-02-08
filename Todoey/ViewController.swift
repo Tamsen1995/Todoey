af2edf41a59d905e73f99daa2acaa8eb8443ed02
Tamsen1995 <Tamanhbui95@gmail.com>
2018-02-08 23:04:32 +0100

    [ADD] new project

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Nguyen Tam Anh Bui on 2/8/18.
+//  Copyright © 2018 Nguyen Tam Anh Bui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
